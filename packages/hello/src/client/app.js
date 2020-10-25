28c017942003cf2b18dc8f9a33effd5693dd3c45
tphanson <tphanson@kambria.io>
2020-10-25 21:53:26 +0700

    add types

--- packages/hello/src/client/app.js
+++ packages/hello/src/client/app.js
@@ -1,36 +1,14 @@
 const { sendAndConfirmTransaction, TransactionInstruction, Transaction } = require('@solana/web3.js');
-const BufferLayout = require('buffer-layout');
-const BN = require('bn.js');
-
-/**
- * Layout of the greeted account data
- */
-const greeterDataLayout = BufferLayout.struct([
-  BufferLayout.u32('numGreets'),
-]);
+const { layout, u8, u32 } = require('../../lib/type');
 
 /**
  * Say hello
  */
-class u32 extends BN {
-  toBuffer() {
-    const a = super.toArray().reverse();
-    const b = Buffer.from(a);
-    if (b.length > 4) throw new Error('u64 too large');
-    if (b.length === 4) return b;
-    const zeroPad = Buffer.alloc(4);
-    b.copy(zeroPad);
-    return zeroPad;
-  }
-}
 async function sayHello(amount, greeterId, programId, payer, connection) {
   console.log('Saying hello to', greeterId.toBase58());
-  const dataLayout = BufferLayout.struct([
-    BufferLayout.u8('instruction'),
-    BufferLayout.blob(4, 'amount'),
-  ]);
-  const data = Buffer.alloc(dataLayout.span);
-  dataLayout.encode({ instruction: 0, amount: new u32(amount).toBuffer() }, data);
+  const _instruction = new u8('instruction', 0);
+  const _amount = new u32('amount', amount);
+  const data = layout(_instruction, _amount);
   const instruction = new TransactionInstruction({
     keys: [{ pubkey: greeterId, isSigner: false, isWritable: true }],
     programId,
@@ -52,8 +30,8 @@ async function sayHello(amount, greeterId, programId, payer, connection) {
 async function reportHellos(greeterId, connection) {
   const accountInfo = await connection.getAccountInfo(greeterId);
   if (!accountInfo) throw new Error('Cannot find the greeted account');
-  const info = greeterDataLayout.decode(Buffer.from(accountInfo.data));
-  console.log(greeterId.toBase58(), 'has been greeted', info.numGreets.toString(), 'times');
+  const info = u32.fromBuffer(accountInfo.data);
+  console.log(greeterId.toBase58(), 'has been greeted', info, 'times');
 }
 
 module.exports = { sayHello, reportHellos }
