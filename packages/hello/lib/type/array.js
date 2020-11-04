c5d7bda063136ba10e366be213129b1d37f62167
tphanson <tphanson@kambria.io>
2020-11-04 17:57:33 +0700

    add tuple

--- packages/hello/lib/type/array.js
+++ packages/hello/lib/type/array.js
@@ -8,13 +8,9 @@ class array {
   constructor(type, value = []) {
     this.value = value;
     this.type = type;
-
-    const { primaryType, len } = this._parseType(this.type);
+    const { primaryType, space } = this._parseType(this.type);
     this._primaryType = primaryType;
-    this._primarySpace = (new types[this._primaryType]).space;
-    this._len = len;
-
-    this.space = this._primarySpace * this._len;
+    this.space = space;
   }
 
   _parseType = (type) => {
@@ -26,10 +22,12 @@ class array {
     if (type.pop() !== ']') throw new Error('Invalid type array');
     type = type.join('');
     let [primaryType, len] = type.split(';');
+    if (!primaryType || !len) throw new Error('Invalid type array');
     if (!types[primaryType]) throw new Error('Invalid type array');
     try { len = parseInt(len) } catch (er) { len = 0 }
     if (len <= 0) throw new Error('Invalid type array');
-    return { primaryType, len }
+    const space = (new types[this._primaryType]).space * len;
+    return { primaryType, space }
   }
 
   toBuffer = () => {
@@ -43,9 +41,9 @@ class array {
     let offset = 0;
     while (offset < this.space) {
       const type = new types[this._primaryType]();
-      type.fromBuffer(buf.slice(offset, offset + this._primarySpace));
+      type.fromBuffer(buf.slice(offset, offset + type.space));
       this.value.push(type.value);
-      offset += this._primarySpace;
+      offset += type.space;
     }
   }
 }
--- packages/hello/lib/type/index.js
+++ packages/hello/lib/type/index.js
@@ -3,6 +3,7 @@ const { char } = require('./char');
 const { u8, u16, u32, u64 } = require('./usize');
 const { i8, i16, i32, i64 } = require('./isize');
 const { array } = require('./array');
+const { tuple } = require('./tuple');
 
 pack = (...types) => {
   return Buffer.concat(types.map(type => type.toBuffer()));
@@ -27,5 +28,6 @@ module.exports = {
   i8, i16, i32, i64,
   u8, u16, u32, u64,
   array,
+  tuple,
   pack, unpack,
 }
