651aebef62f64e3edfaad66d8a8266e0b21cdc65
tphanson <tphanson@kambria.io>
2020-11-08 18:42:15 +0700

    restructure

--- packages/hello/src/client/index.js
+++ packages/hello/src/client/index.js
@@ -1,5 +1,12 @@
-const { sendAndConfirmTransaction, TransactionInstruction, Transaction } = require('@solana/web3.js');
+const {
+  sendAndConfirmTransaction,
+  TransactionInstruction,
+  Transaction,
+  PublicKey
+} = require('@solana/web3.js');
 const soproxABI = require('../../lib/soprox-abi');
+const { establishConnection, loadPayerFromStore } = require('../../lib/network');
+const store = require('../../lib/store');
 
 /**
  * Say hello
@@ -38,4 +45,27 @@ const reportHello = async (register, connection) => {
   return layout.value;
 }
 
-module.exports = { sayHello, reportHello }
+const init = async () => {
+  const connection = await establishConnection();
+  const payer = loadPayerFromStore();
+  const program = store.load('program');
+  const programId = new PublicKey(program.address);
+  const registers = store.load('abi').schema.map(register => {
+    register.id = new PublicKey(register.address);
+    return register;
+  });
+  return { connection, payer, programId, registers }
+}
+
+const main = async () => {
+  console.log("Let's say hello to a Solana account...");
+  const { connection, payer, programId, registers: [register] } = await init();
+  let data = await reportHello(register, connection);
+  console.log('Current data:', data);
+  await sayHello(1, !data.toggleState, register, programId, payer, connection);
+  data = await reportHello(register, connection);
+  console.log('New data:', data);
+  console.log('Success');
+}
+
+try { main() } catch (er) { console.error(er) }
